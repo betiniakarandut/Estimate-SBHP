8cc4f98f409e497331a7d90a59ab6633f8658e3c
betiniakarandut <betiniakarandut@gmail.com>
2024-01-23 16:13:18 +0100

    Navigate to Login/Register onClick Sign Out

--- client/src/Components/Navigation.jsx
+++ client/src/Components/Navigation.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export const Navigation = ({ onRouteChange, isloggedIn }) => {
+  const navigate = useNavigate()
   const handleSignOut = () => {
+    navigate('/');
     onRouteChange('logout');
   };
 
