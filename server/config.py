83d7d077d930cdef0ce14c44601e115e82a47251
betiniakarandut <betiniakarandut@gmail.com>
2024-01-28 21:41:18 +0100

    Added a Procfile

--- server/config.py
+++ server/config.py
@@ -1,26 +1,25 @@
-from dotenv import load_dotenv
 import os
-import secrets
 from urllib.parse import quote
 import redis
 
-load_dotenv()
 
 class ApplicationConfig:
-    password = os.getenv("DB_PASSWORD")
+    """Configuration settings"""
+    password = os.environ.get("DB_PASSWORD")
     encoded_password = quote(password)
 
     SQLALCHEMY_TRACK_MODIFICATION = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = f"mysql://root:{encoded_password}@localhost/SBHP_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # f"mysql://root:{encoded_password}@localhost/SBHP_db"
 
     SESSION_TYPE = 'redis'
     SESSION_PERMANENT = True
     SESSION_KEY_PREFIX = '2ew'
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url(os.environ.get("REDIS_URL"))
 
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
   
 
    
