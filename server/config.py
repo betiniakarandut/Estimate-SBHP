6e11bb8fe40783903d2db9603cc65fe33470eb13
betiniakarandut <betiniakarandut@gmail.com>
2024-02-02 11:24:28 +0100

    Removed redundant codes

--- server/config.py
+++ server/config.py
@@ -1,21 +1,17 @@
 import os
-from urllib.parse import quote
 import redis
 
 
 class ApplicationConfig:
     """Configuration settings"""
-    password = os.environ.get("DB_PASSWORD")
-    encoded_password = quote(password)
+    db_password = os.environ.get("DB_PASSWORD")
 
     SQLALCHEMY_TRACK_MODIFICATION = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # f"mysql://root:{encoded_password}@localhost/SBHP_db"
-
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{db_password}@localhost:5432/SBHP_db"
+    
     SESSION_TYPE = 'redis'
-    SESSION_PERMANENT = True
-    SESSION_KEY_PREFIX = '2ew'
+    SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url(os.environ.get("REDIS_URL"))
 
